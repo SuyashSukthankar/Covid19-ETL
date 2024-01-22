89927f5c39f2f497ff3921b87201dd6c52b48842
SuyashSukthankar <suyash.sukthankar@gmail.com>
2024-01-22 12:30:46 -0600

    Update copyGitHubS3_lambda.py

--- copyGitHubS3_lambda.py
+++ copyGitHubS3_lambda.py
@@ -14,33 +14,7 @@ def lambda_handler(event, context):
     def get_s3_object_count(bucket_name,key):
         key_count = s3.list_objects_v2(Bucket=bucket_name, Prefix=key)['KeyCount']
         return key_count
-    #confirmed_response = s3.get_object(Bucket='projectpro-covid19-test-data', Key='covid19/confirmed/time_series_covid19_confirmed_global.csv')
-    #confirmed_file_content = confirmed_response['Body'].read().decode('utf-8')
-    #s3.put_object(Bucket='projectpro-covid19-test-data', Key='covid19/archival/confirmed/'+date+'/time_series_covid19_confirmed_global.csv', Body=confirmed_file_content)
-    
-    #confirmed_url = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
-    #with urllib.request.urlopen(confirmed_url) as response:
-    #    data = response.read()
-    #s3.put_object(Bucket='projectpro-covid19-test-data', Key='covid19/confirmed/time_series_covid19_confirmed_global.csv', Body=data)
-    
-    #death_response = s3.get_object(Bucket='projectpro-covid19-test-data', Key='covid19/death/time_series_covid19_deaths_global.csv')
-    #death_file_content = death_response['Body'].read().decode('utf-8')
-    #s3.put_object(Bucket='projectpro-covid19-test-data', Key='covid19/archival/death/'+date+'/time_series_covid19_deaths_global.csv', Body=death_file_content)
-    
-    #death_url = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
-    #with urllib.request.urlopen(death_url) as response:
-    #    data = response.read()
-    #s3.put_object(Bucket='projectpro-covid19-test-data', Key='covid19/death/time_series_covid19_deaths_global.csv', Body=data)
-    
-    #recovered_response = s3.get_object(Bucket='projectpro-covid19-test-data', Key='covid19/recovered/time_series_covid19_recovered_global.csv')
-    #recovered_file_content = recovered_response['Body'].read().decode('utf-8')
-    #s3.put_object(Bucket='projectpro-covid19-test-data', Key='covid19/archival/recovered/'+date+'/time_series_covid19_recovered_global.csv', Body=recovered_file_content)
-    
-    #recovered_url = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
-    #with urllib.request.urlopen(recovered_url) as response:
-    #    data = response.read()
-    #s3.put_object(Bucket='projectpro-covid19-test-data', Key='covid19/recovered/time_series_covid19_recovered_global.csv', Body=data)
-    
+   
     covid_array = ['confirmed','deaths','recovered']
     
     for element in covid_array:
